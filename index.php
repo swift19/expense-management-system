37ede323ec7b149cd33f05c04919fec4e2331e70
Geraldo Bathan <geraldbathan24@gmail.com>
2020-04-01 23:13:35 +0800

    login-form

--- index.php
+++ index.php
@@ -36,34 +36,35 @@ if(isset($_POST['login']))
 	<hr />
 		<div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
 			<div class="login-panel panel panel-default">
-				<div class="panel-heading">Log in</div>
+				<div class="panel-heading">Login</div>
 				<div class="panel-body">
-					<p style="font-size:16px; color:red" align="center"> <?php if($msg){
-    echo $msg;
-  }  ?> </p>
+					<p style="font-size:16px; color:red" align="center"> <?php if($msg){ echo $msg; }  ?> </p>
 					<form role="form" action="" method="post" id="" name="login">
 						<fieldset>
 							<div class="form-group">
 								<input class="form-control" placeholder="E-mail" name="email" type="email" autofocus="" required="true">
-							</div>
-							<a href="forgot-password.php">Forgot Password?</a>
+							</div>							
 							<div class="form-group">
 								<input class="form-control" placeholder="Password" name="password" type="password" value="" required="true">
+								<a href="forgot-password.php">Having trouble signing in?</a>
 							</div>
-							<div class="checkbox">
-								<button type="submit" value="login" name="login" class="btn btn-primary">login</button><span style="padding-left:250px">
-								<a href="register.php" class="btn btn-primary">Register</a></span>
+							<div class="form-group">
+								<button type="submit" value="login" name="login" class="btn btn-primary"  style="width:100%">Login</button>		
 							</div>
-							</fieldset>
+						</fieldset>
+						<hr/>
+						<div align='center'>
+						   <label>Don't have an Account yet? <a href="register.php" >   Create an account</a></label>
+						</div>							
 					</form>
 				</div>
 				<div></div>
 			</div>
-		</div><!-- /.col-->
-	</div><!-- /.row -->	
+		</div>
+	</div>	
 	
 
 <script src="js/jquery-1.11.1.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
 </body>
 </html>
